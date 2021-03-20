18daa258bbdb2765ccb67860b2032727b7c4e7c4
bhbranno <bhbranno@hawaii.edu>
2021-03-20 09:08:28 -1000

    commit

--- Lab8/Ex3.js
+++ Lab8/Ex3.js
@@ -2,11 +2,8 @@ require("./products_data.js");
 
 num_products = 5;
 
-item_num = 1;
 
-while(item_num <= (num_products/2)) {
+for(var item_num = 1; eval("typeof name"+item_num) != 'undefined'; item_num++) {
     console.log(`${item_num}. ${eval('name' + item_num)}`);
-    item_num++;
-
 }
 console.log('That\'s all we have!')
