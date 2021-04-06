6c69fa4642f472ce511135574716560c29fdf679
bhbranno <bhbranno@hawaii.edu>
2021-04-05 20:21:25 -1000

    commit

---
+++ Brannon_Ben_Assignment1/products_data.js
@@ -0,0 +1,34 @@
+// Author: Ben Brannon
+// create products json array
+var products = [
+  {
+    "product": "US Military Survival Handbook",
+    "price": 6.99,
+    "image": './images/manual.jpg',
+    "description": "Become a survival expert with the same surival manual used and trusted by the US Military."
+  },
+  {
+    "product": "Woobie 3-in-1 Survival Blanket",
+    "price": 39.99,
+    "image": './images/woobie.jpg',
+    "description": "Stay warm and dry out in the field with this 3-in-1 survival blanket. Can be used as a sleeping bag, poncho, and blanket."
+  },
+  {
+    "product": "Camo Face Paint",
+    "price": 2.99,
+    "image": './images/paint.jpg',
+    "description": "Stay hidden from your enemies and blend in to your environment with this US Army Camo Face Paint."
+  },
+  {
+    "product": "6 in 1 LED Survival Whistle Kit",
+    "price": 4.99,
+    "image": './images/whistle.jpg',
+    "description": "Features include LED light, signal mirror, thermometer, compass, magnifying glass, and whistle."
+  },
+  {
+    "product": "Stormproof Matches",
+    "price": 2.75,
+    "image": './images/matches.jpg',
+    "description": "When surviving in the wild, a fire is a necessity. Come rain or shine, these matches can be trusted to ignite and get your fire started."
+  }
+]
---
+++ Brannon_Ben_Assignment1/server.js
@@ -0,0 +1,7 @@
+// code taken from Assignment 1 instructions
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('.'));
+app.listen(8080, () => console.log(`listening on port 8080`));
